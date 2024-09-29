00a05c098fcd7d88b3b987c668551be7af9b832b
Lehana44 <lehanalebopo@gmail.com>
2024-09-29 06:00:53 +0200

    Landing Page1

---
+++ Tiger-Art-Website-44-/script.js
@@ -0,0 +1,7 @@
+/***************************************Navbar Section********************************************************/
+document.getElementById('hamburger').addEventListener('click', function() {
+    const navLinks = document.getElementById('navLinks');
+    navLinks.classList.toggle('active');
+});
+
+/***************************************Navbar Section********************************************************/
